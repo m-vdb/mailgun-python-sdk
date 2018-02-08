69a6d5bb8cdf4a93d10ac19314172df9c252b907
m-vdb <mvergerdelbove@work4labs.com>
2018-02-08 22:50:34 +0100

    add basic setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Mailgun SDK',
+    version='0.1',
+    description='Mailgun Python SDK (unofficial)',
+    author='Maxime Vdb',
+    packages=find_packages(),
+    install_requires=['requests'],
+    license="MIT",
+    keywords="mailgun api sdk",
+    project_urls={
+        "Source Code": "https://github.com/m-vdb/mailgun-python-sdk",
+    }
+)
