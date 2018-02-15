55347aeba539750a2ec4cd36a234f9e2ada3cf1c
m-vdb <mvergerdelbove@work4labs.com>
2018-02-15 08:59:49 +0100

    rename package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Mailgun SDK',
+    name='mailgun-python-sdk',
     version='0.1',
     description='Mailgun Python SDK (unofficial)',
     author='Maxime Vdb',
